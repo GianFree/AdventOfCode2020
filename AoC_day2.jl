439eb1eb8362f9fa0666bdd40a042a8a03f29961
Gianfranco Abrusci <abrusci.gianfranco@gmail.com>
2020-12-08 15:18:37 +0100

    add template day2, fixed relative path in day1

--- AoC_day2.jl
+++ AoC_day2.jl
@@ -20,32 +20,32 @@ We want to read the input file to solve the first advent of code.
 # ╔═╡ 74a02316-387a-11eb-0f44-59adc2349951
 function read_data(filename)
 	"""
-	We want to read the file provided.
+	We want to read the file provided. 
 	It returns an array of string that we have to convert.
 	"""
 	open(filename) do io
 		readlines(io)
-	end
+	end	
 end
 
 # ╔═╡ e78a5ea0-387d-11eb-18b3-6372f503db67
 function get_day1_data(filename, kind=Int64)
 	"""
-	Return an array with the proper type.
+	Return an array with the proper type. 
 	Default `Int64`.
 	"""
 	return [parse(kind, num) for num in read_data(filename)]
 end
 
 # ╔═╡ df54fe9e-387c-11eb-1388-533d38d1fee5
-a = get_day1_data("input_files/day1_input.dat")
+a = get_day1_data("day1_input.csv")
 
 # ╔═╡ ef11d8e4-387b-11eb-18f8-15f500f76ff7
 # Number of elements
 N = length(a)
 
 # ╔═╡ 37bad250-387f-11eb-0b3d-b31f046a1030
-let
+let 
 	x = nothing
 	for i in 1:N-1
 		for j in i+1:N
@@ -73,7 +73,7 @@ Let's try the brute force first.
 """
 
 # ╔═╡ c8e2cce0-3881-11eb-18fc-1bf74ab9301c
-let
+let 
 	x = nothing
 	for i in 1:N-2
 		for j in i+1:N-1
